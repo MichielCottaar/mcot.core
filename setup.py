fa7aecc00c4164688390d276c8e9fe73a72aff22
Michiel Cottaar <MichielCottaar@protonmail.com>
2020-09-13 15:03:34 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class doc(Command):
 
 ###################################################################
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 KEYWORDS = []
 CLASSIFIERS = [
     'Development Status :: 2 - Pre-Alpha',
