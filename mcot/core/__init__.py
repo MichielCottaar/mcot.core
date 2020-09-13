c0efd162d708edc2d9a50b350e3bc3c0d00430a7
Michiel Cottaar <MichielCottaar@protonmail.com>
2020-09-13 13:09:08 +0100

    also store version in core __init__.py

--- mcot/core/__init__.py
+++ mcot/core/__init__.py
@@ -1,5 +1,6 @@
 """Package for mcot.core."""
 from ._write_gifti import write_gifti
 from . import surface, cifti, greyordinate, log, scripts, spherical
+__version__ = '0.2.1'
 scripts.directories.add(f'{__name__}._scripts', group=None)
 
