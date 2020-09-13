9b09b3b0c572b48da1a43849f457a8d916650fc9
Michiel Cottaar <MichielCottaar@protonmail.com>
2020-09-13 15:21:22 +0100

    BUG: fix circular import

--- mcot/core/__init__.py
+++ mcot/core/__init__.py
@@ -1,6 +1,6 @@
 """Package for mcot.core."""
+from . import surface, cifti, greyordinate, log, scripts, spherical, _write_gifti
 from ._write_gifti import write_gifti
-from . import surface, cifti, greyordinate, log, scripts, spherical
 __version__ = '0.2.2'
 scripts.directories.add(f'{__name__}._scripts', group=None)
 
